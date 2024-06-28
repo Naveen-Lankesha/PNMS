66238d0b9e3650c480e56858f6d5f6195acdb802
Nikini <karunarathnanikini@gmail.com>
2024-06-28 11:20:36 +0530

    quantity v0.5

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -13,20 +13,38 @@ const StoreContextProvider = (props) => {
   };
 
   const addToCart = (itemId) => {
-    if (!cartItems[itemId]) {
-      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
-    } else {
-      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
-    }
+    setCartItems((prev) => ({
+      ...prev,
+      [itemId]: (prev[itemId] || 0) + 1,
+    }));
   };
 
   const removeFromCart = (itemId) => {
-    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    setCartItems((prev) => {
+      const newCartItems = { ...prev };
+      if (newCartItems[itemId] > 1) {
+        newCartItems[itemId] -= 1;
+      } else {
+        delete newCartItems[itemId];
+      }
+      return newCartItems;
+    });
   };
 
-  // useEffect(() => {
-  //   console.log(cartItems);
-  // }, [cartItems]);
+  useEffect(() => {
+    fetch("http://localhost:5001/all-items")
+      .then((res) => res.json())
+      .then((data) => {
+        const initialCartItems = {};
+        data.forEach((item) => {
+          if (item.quantity > 0) {
+            initialCartItems[item._id] = item.quantity;
+          }
+        });
+        setCartItems(initialCartItems);
+      });
+  }, []);
+
 
   const getTotalCartAmount = () => {
     let totalAmount = 0;
@@ -105,3 +123,22 @@ export default StoreContextProvider;
 // };
 
 // export default StoreContextProvider;
+
+
+
+//NIKINI
+// const addToCart = (itemId) => {
+//   if (!cartItems[itemId]) {
+//     setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+//   } else {
+//     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+//   }
+// };
+
+// const removeFromCart = (itemId) => {
+//   setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+// };
+
+// useEffect(() => {
+//   fetch("http://localhost:5001/all-items").then(res => res.json()).then(data => console.log(data))
+//  }, []);
