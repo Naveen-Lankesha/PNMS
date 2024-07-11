36739da154af07901d72da88a5d7b51a05618f5b
G.H.N. Lankesha <136252384+Naveen-Lankesha@users.noreply.github.com>
2024-07-11 23:44:00 +0530

    inventory -front

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,50 +1,57 @@
 import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
-import { shoe_list } from "../assets/frontend_assets/assets";
+//import { shoe_list } from "../assets/frontend_assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+  const url = "http://localhost:4000";
+  const [token, setToken] = useState("");
+  const [shoe_list, setShoeList] = useState([]);
+
   const [cartItems, setCartItems] = useState({});
-  const [size, setSize] = useState({}); // Update the initial state to be an object
 
-  const handleChange = (id, event) => {
+  const handleChange = async (id, event) => {
     setSize((prevSize) => ({ ...prevSize, [id]: event.target.value }));
+    if (token) {
+      await axios.post(
+        url + "/api/inventory/updateShoeSize",
+        { itemId: id, size: event.target.value },
+        { headers: { token } }
+      );
+    }
   };
 
-  const addToCart = (itemId) => {
-    setCartItems((prev) => ({
-      ...prev,
-      [itemId]: (prev[itemId] || 0) + 1,
-    }));
+  const addToCart = async (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+    if (token) {
+      await axios.post(
+        url + "/api/inventory/add",
+        { itemId },
+        { headers: { token } }
+      );
+    }
   };
 
-  const removeFromCart = (itemId) => {
-    setCartItems((prev) => {
-      const newCartItems = { ...prev };
-      if (newCartItems[itemId] > 1) {
-        newCartItems[itemId] -= 1;
-      } else {
-        delete newCartItems[itemId];
-      }
-      return newCartItems;
-    });
+  const removeFromCart = async (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+    if (token) {
+      await axios.post(
+        url + "/api/inventory/remove",
+        { itemId },
+        { headers: { token } }
+      );
+    }
   };
 
   // useEffect(() => {
-  //   fetch("http://localhost:5001/all-items")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       const initialCartItems = {};
-  //       data.forEach((item) => {
-  //         if (item.quantity > 0) {
-  //           initialCartItems[item._id] = item.quantity;
-  //         }
-  //       });
-  //       setCartItems(initialCartItems);
-  //     });
-  // }, []);
-
+  //   console.log(cartItems);
+  // }, [cartItems]);
 
   const getTotalCartAmount = () => {
     let totalAmount = 0;
@@ -55,8 +62,50 @@ const StoreContextProvider = (props) => {
     return totalAmount;
   };
 
+  // fetch data from backend
+  const fetchShoeList = async () => {
+    const response = await axios.get(`${url}/api/item/list`);
+    setShoeList(response.data.data);
+  };
+
+  //to keep the items in the cart after refresh
+  const loadCartData = async (token) => {
+    const response = await axios.post(
+      `${url}/api/inventory/get`,
+      {},
+      {
+        headers: { token },
+      }
+    );
+    setCartItems(response.data.cartData);
+  };
+
+  //to keep the shoeSize in the cart after refresh
+  const loadShoeSize = async (token) => {
+    const response = await axios.post(
+      `${url}/api/inventory/getShoeSize`,
+      {},
+      {
+        headers: { token },
+      }
+    );
+    setSize(response.data.shoeSize);
+  };
+
+  //prevet refresh logout
+  useEffect(() => {
+    async function fetchData() {
+      await fetchShoeList();
+      if (localStorage.getItem("token")) {
+        setToken(localStorage.getItem("token"));
+        await loadCartData(localStorage.getItem("token")); //to keep the items in the cart after refresh
+        await loadShoeSize(localStorage.getItem("token")); //to keep the shoeSize in the cart after refresh
+      }
+    }
+    fetchData();
+  }, []);
+
   const contextValue = {
-    size,
     handleChange,
     shoe_list,
     cartItems,
@@ -64,6 +113,9 @@ const StoreContextProvider = (props) => {
     addToCart,
     removeFromCart,
     getTotalCartAmount,
+    url,
+    token,
+    setToken,
   };
 
   return (
@@ -74,6 +126,87 @@ const StoreContextProvider = (props) => {
 };
 
 export default StoreContextProvider;
+
+//----------------------------------------------------
+
+// import React, { createContext, useEffect, useState } from "react";
+// import axios from "axios";
+
+// import { shoe_list } from "../assets/frontend_assets/assets";
+
+// export const StoreContext = createContext(null);
+
+// const StoreContextProvider = (props) => {
+//   const [cartItems, setCartItems] = useState({});
+//   const [size, setSize] = useState({}); // Update the initial state to be an object
+
+//   const handleChange = (id, event) => {
+//     setSize((prevSize) => ({ ...prevSize, [id]: event.target.value }));
+//   };
+
+//   const addToCart = (itemId) => {
+//     setCartItems((prev) => ({
+//       ...prev,
+//       [itemId]: (prev[itemId] || 0) + 1,
+//     }));
+//   };
+
+//   const removeFromCart = (itemId) => {
+//     setCartItems((prev) => {
+//       const newCartItems = { ...prev };
+//       if (newCartItems[itemId] > 1) {
+//         newCartItems[itemId] -= 1;
+//       } else {
+//         delete newCartItems[itemId];
+//       }
+//       return newCartItems;
+//     });
+//   };
+
+//   // useEffect(() => {
+//   //   fetch("http://localhost:5001/all-items")
+//   //     .then((res) => res.json())
+//   //     .then((data) => {
+//   //       const initialCartItems = {};
+//   //       data.forEach((item) => {
+//   //         if (item.quantity > 0) {
+//   //           initialCartItems[item._id] = item.quantity;
+//   //         }
+//   //       });
+//   //       setCartItems(initialCartItems);
+//   //     });
+//   // }, []);
+
+//   const getTotalCartAmount = () => {
+//     let totalAmount = 0;
+//     for (const item in cartItems) {
+//       let itemInfo = shoe_list.find((product) => product._id === item);
+//       totalAmount += itemInfo.price * cartItems[item];
+//     }
+//     return totalAmount;
+//   };
+
+//   const contextValue = {
+//     size,
+//     handleChange,
+//     shoe_list,
+//     cartItems,
+//     setCartItems,
+//     addToCart,
+//     removeFromCart,
+//     getTotalCartAmount,
+//   };
+
+//   return (
+//     <StoreContext.Provider value={contextValue}>
+//       {props.children}
+//     </StoreContext.Provider>
+//   );
+// };
+
+// export default StoreContextProvider;
+
+//------------------------------------------
 
 // import React, { createContext, useEffect, useState } from "react";
 
@@ -123,8 +256,6 @@ export default StoreContextProvider;
 // };
 
 // export default StoreContextProvider;
-
-
 
 //NIKINI
 // const addToCart = (itemId) => {
