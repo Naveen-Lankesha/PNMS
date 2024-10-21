e9d7a1618a008c29472271d82fe991bf8d1d8e23
G.H.N. Lankesha <136252384+Naveen-Lankesha@users.noreply.github.com>
2024-10-22 01:19:01 +0530

    Docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt(process.env.VITE_PORT ?? "3003"),
+    port: parseInt(process.env.VITE_PORT ?? "5173"),
   },
 });
